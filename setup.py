a5b0ca065672aef673149ca3f4451440d9a96cc7
Rob Hall <rahall@colorado.edu>
2018-06-29 13:14:25 -0600

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Kaggle Competition - Home Credit Default Risk',
+    author='Rob Hall',
+    license='',
+)
