dc9bd10e7f10f27d402580883de4dd92646e4a0c
Rob Hall <rahall@colorado.edu>
2018-06-29 21:09:36 -0600

    First part of cleaning

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -2,81 +2,34 @@
 ## Modules for feature cleaning Titanic dataset
 import pandas as pd
 
-def simplify_sex(df):
-    """Simplify sex and one hot encode them"""
+def simplifyEducation(dfIn, dfOut):
+    """Update education and one hot encode them"""
 
-    sex = pd.get_dummies(df.Sex, prefix = 'Sex')
-    df = pd.concat([df, sex], axis = 1)
-    return df
+    edu = pd.get_dummies(dfIn.NAME_EDUCATION_TYPE, prefix = 'EDU')
+    dfOut = pd.concat([dfOut, edu], axis = 1)
+    return dfOut
 
-def simplify_class(df):
-    """Simplify passenger class and one hot encode them"""
+def simplifyFamily(dfIn, dfOut):
+    """Update Family and one hot encode them"""
 
-    new_class = pd.get_dummies(df.Pclass, prefix = 'Class')
-    df = pd.concat([df, new_class], axis = 1)
-    return df
+    fam = pd.get_dummies(dfIn.NAME_FAMILY_STATUS, prefix = 'FAM')
+    dfOut = pd.concat([dfOut, fam], axis = 1)
+    return dfOut
 
-def simplify_embarked(df):
-    """Simplify where passengers embared from and one hot encode them"""
 
-    embarked = pd.get_dummies(df.Embarked , prefix='Embarked' )
-    df = pd.concat([df, embarked], axis=1)
-    return df
+def simplifyIncome(dfIn, dfOut):
+    "Update Income and one hot encode them"
 
-def simplify_ages(df):
-    """Seperate ages into bins and one hot encode them"""
+    inc = pd.get_dummies(dfIn.NAME_INCOME_TYPE, prefix='INC')
+    dfOut = pd.concat([dfOut, inc], axis=1)
+    return dfOut
 
-    bins = (-1, 0,5,12,18,25,35,60,120)
-    df.Age = df.Age.fillna(-0.5)
-    group_names = ['Unknown', 'Baby', 'Child',
-                   'Teenager', 'Student', 'Young Adult', 'Adult', 'Senior']
-    df.Age = pd.cut(df.Age, bins, labels = group_names)
-    ages = pd.get_dummies(df.Age, prefix = 'Age')
-    df = pd.concat([df,ages], axis = 1)
-    return df
 
-def simplify_fares(df):
-    """Simplify fares and one hot encode them"""
+def execute_cleaning(dfIn, dfOut):
+    """One education, family, income."""
 
-    df.Fare = df.Fare.fillna(-0.5)
-    bins = (-1, 0, 8, 15, 31, 1000)
-    group_names = ['Unknown', '1_quartile', '2_quartile', '3_quartile', '4_quartile']
-    df.Fare = pd.cut(df.Fare, bins, labels=group_names)
-    fare = pd.get_dummies(df.Fare, prefix = 'Fare')
-    df = pd.concat([df, fare], axis = 1)
-    return df
+    dfOut = simplifyEducation(dfIn, dfOut)
+    dfOut = simplifyFamily(dfIn, dfOut)
+    dfOut = simplifyIncome(dfIn, dfOut)
 
-def simpFamSize(df):
-    bins = (0, 2, 4, 8, 20)
-    group_names = ['Small', 'Medium', 'Large', 'ExtLarge']
-    df.SibSp = pd.cut(df.SibSp, bins, labels = group_names)
-    sibsp = pd.get_dummies(df.SibSp, prefix = 'Sibs')
-    df = pd.concat([df, sibsp], axis = 1)
-    return df
-
-def drop_features(df):
-    """Drop extra features not engineered"""
-
-    return df.drop(['Ticket', 'Name', 'Age', 'Embarked', 'Pclass', 'Sex', 'Cabin', 'Fare', 'SibSp'], axis=1)
-
-def produce_columns(df):
-    """Reports columns for use in model."""
-    columnsNames = []
-    for i in df.columns:
-        if i == 'Survived' or i == 'PassengerId':
-            pass
-        else:
-            columnsNames.append(i)
-    return columnsNames
-
-def execute_cleaning(df):
-    """One hot encode age, embarked, fares, class, sex and drop extra features."""
-
-    df = simplify_ages(df)
-    df = simplify_embarked(df)
-    df = simplify_fares(df)
-    df = simplify_class(df)
-    df = simplify_sex(df)
-    df = simpFamSize(df)
-    df = drop_features(df)
-    return df
+    return dfOut
