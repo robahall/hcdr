b7fc86a88e2ad5f3697006032ea2a17c70a87a2e
Rob Hall <rahall@colorado.edu>
2018-06-30 14:04:31 -0600

    added engineering of number of days of work. then standardized the values

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -2,6 +2,8 @@
 ## Modules for feature engineering hcdr dataset
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import scale
 
 def daysToYears(dfIn, dfOut):
     """Update education and one hot encode them"""
@@ -22,18 +24,25 @@ def normalizeIncome(dfIn, dfOut):
 
 def standardizedIncome(dfIn, dfOut):
     """Log transform income and standardize"""
-    nIncome = pd.Series(preprocessing.scale(np.log(dfIn['AMT_INCOME_TOTAL'])), name = 'scaledLogINC')
+    nIncome = pd.Series(scale(np.log(dfIn['AMT_INCOME_TOTAL'])), name = 'scaledLogINC')
     dfOut = pd.concat([dfOut, nIncome], axis = 1)
     return dfOut
+
 
 def engineerDays(dfIn, dfOut):
     '''DAYS_EMPLOYEED is from when employment starts. Data is positively skewed.
     Need to log transform. Added flag columns for the anomly in data and people who have no job (only 2 in train set) .'''
+
+    imp = Imputer(missing_values='NaN', strategy='mean', axis=1)
+
     dfOut['DAYS_EMPLOYED_ANOM'] = dfIn["DAYS_EMPLOYED"] == 365243
     dfOut['DAYS_EMPLOYED_ZERO'] = dfIn["DAYS_EMPLOYED"] == 0
     dfOut['DAYS_EMPLOYED'] = dfIn['DAYS_EMPLOYED'].replace({0: np.nan})
     dfOut['DAYS_EMPLOYED'] = (dfOut['DAYS_EMPLOYED'] * (-1)).apply(np.log)
     dfOut['DAYS_EMPLOYED'] = dfOut['DAYS_EMPLOYED'].replace({np.log(365243): np.nan})
+    dfOut['DAYS_EMPLOYED'] = dfOut['DAYS_EMPLOYED'].fillna(dfOut['DAYS_EMPLOYED'].mean())
+    dfOut['DAYS_EMPLOYED'] = scale(dfOut['DAYS_EMPLOYED'])
+
     return dfOut
 
 def simplifyEducation(dfIn, dfOut):
