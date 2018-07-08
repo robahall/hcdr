e692b383193db1e9f22ef9ad5c5916ff432c73d1
Rob Hall <rahall@colorado.edu>
2018-07-08 11:58:02 -0600

    Updating cleaning data in src

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -12,6 +12,7 @@ def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
+
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
@@ -23,7 +24,7 @@ if __name__ == '__main__':
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
 
-    # find .env automagically by walking up directories until it's found, then
+    # find .env automatically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
